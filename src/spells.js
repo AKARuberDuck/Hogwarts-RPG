4143557bed90b39e661ee673caecac4df5e6d2cf
AKARuberDuck <ss2875356@gmail.com>
2025-06-16 10:03:48 -0500

    Update spells.js

--- src/spells.js
+++ src/spells.js
@@ -1,1 +1,12 @@
+const spells = {
+    "Expelliarmus": "Disarms your opponent!",
+    "Lumos": "Lights up dark areas!",
+    "Alohomora": "Unlocks doors!",
+    "Expecto Patronum": "Summons your Patronus!"
+};
 
+function castSpell(spellName) {
+    return spells[spellName] ? `✨ You cast ${spellName}! ${spells[spellName]}` : "❌ Invalid spell!";
+}
+
+module.exports = { spells, castSpell };
