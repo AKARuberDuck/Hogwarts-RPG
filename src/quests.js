2d52e46a9fd0d361f4b8e37bb2e6f934450de6e5
AKARuberDuck <ss2875356@gmail.com>
2025-06-16 10:04:40 -0500

    Update quests.js

--- src/quests.js
+++ src/quests.js
@@ -1,1 +1,15 @@
+const years = {
+    1: "Discover Hogwarts & defeat Quirrell",
+    2: "Uncover the mystery of the Chamber of Secrets",
+    3: "Protect Hogwarts from Dementors",
+    4: "Compete in the Triwizard Tournament",
+    5: "Join Dumbledore’s Army against Umbridge",
+    6: "Investigate Voldemort's Horcruxes",
+    7: "Defend Hogwarts in the final battle"
+};
 
+function getYearQuest(year) {
+    return years[year] ? `📖 Year ${year}: ${years[year]}` : "❌ Invalid year!";
+}
+
+module.exports = { getYearQuest };
