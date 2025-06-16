effac0e77e4240df3818a427376f3ee5ae302f57
AKARuberDuck <ss2875356@gmail.com>
2025-06-16 10:06:49 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const { sortPlayer } = require("./sorting");
+const { assignWand } = require("./wand");
+const { castSpell } = require("./spells");
 
+const app = express();
+
+app.get("/sort", (req, res) => res.send(sortPlayer(["Gryffindor", "Ravenclaw"])));
+app.get("/wand", (req, res) => res.send(assignWand()));
+app.get("/spell/:name", (req, res) => res.send(castSpell(req.params.name)));
+
+app.listen(3000, () => console.log("🚀 Hogwarts RPG is running!"));
