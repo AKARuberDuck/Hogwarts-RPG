663486f9a4e85d7e207b9cb1560444dae5cef1d4
AKARuberDuck <ss2875356@gmail.com>
2025-06-16 10:05:48 -0500

    Update fullscreen.js

--- src/fullscreen.js
+++ src/fullscreen.js
@@ -1,1 +1,15 @@
+function toggleFullscreen() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    }
+}
 
+document.addEventListener("keydown", function(event) {
+    if (event.key.toLowerCase() === "f") {  
+        toggleFullscreen();
+    }
+});
+
+document.getElementById("fullscreenButton").addEventListener("click", toggleFullscreen);
