cdfe96ce88fb9693a0c287482dd14be4747c7b65
AKARuberDuck <ss2875356@gmail.com>
2025-06-16 10:05:20 -0500

    Update quidditch.js

--- src/quidditch.js
+++ src/quidditch.js
@@ -1,1 +1,11 @@
+const positions = ["Seeker", "Beater", "Keeper", "Chaser"];
 
+function quidditchMatch(position) {
+    if (position === "Seeker") return "⚡ You chase the Golden Snitch!";
+    else if (position === "Beater") return "🏏 You defend teammates with Bludgers!";
+    else if (position === "Keeper") return "🥅 You guard the goalposts!";
+    else if (position === "Chaser") return "🏆 You pass the Quaffle to score!";
+    return "❌ Invalid position!";
+}
+
+module.exports = { positions, quidditchMatch };
